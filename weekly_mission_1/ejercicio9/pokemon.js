4609734a6a8460bda5ff95b0e442428f5ca0ff87
José Luis LM <99145978+JoseLuisLopezManuel@users.noreply.github.com>
2022-04-12 18:36:08 -0500

    Add files via upload

---
+++ weekly_mission_1/ejercicio9/pokemon.js
@@ -0,0 +1,19 @@
+class Pokemon {
+  constructor(name) {
+    this.name = name
+  }
+ // sayHello() {
+ //   console.log(`Mi pokemon ${this.name} te saluda!!!`)
+  //}
+  //sayMessage(message) {
+   // console.log(`Mi pokemon ${this.name} te dice: ${message}`)
+  //}
+  sayHello = () =>{
+    console.log(`Mi pokemon ${this.name} te saluda!!!`)
+  }
+  sayMessage = (message) =>{
+    console.log(`Mi pokemon ${this.name} te dice ${message}`)
+  }
+}
+
+module.exports = Pokemon
