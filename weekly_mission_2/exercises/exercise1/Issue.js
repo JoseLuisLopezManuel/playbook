f8e7ce9dad810c9a107520dceb653c93df9cee85
Jose Luis <launchx06519@innovaccion.mx>
2022-04-15 18:30:24 -0500

    Agregando Issue.js

---
+++ weekly_mission_2/exercises/exercise1/Issue.js
@@ -0,0 +1,19 @@
+const Issue = {
+    title: "Comentarios",
+    repositoryNameAssociated: "LaunchX",
+    status: "Open",
+    numbersOfComments: 250,
+    labels: ['Semana 1','Semana 2', 'Semana 3'],
+    author: "Jose Luis",
+    dateCreated: new Date(),
+    lastUpdate: new Date(),
+    getTitleAndAuthor: function(){
+        return this.title, this.author
+    },
+    getGeneralInfo(){
+        return `El repositoro ${this.repositoryNameAssociated} status: ${this.status}, fue creado: ${this.dateCreated} y guardado ${this.lastUpdate}`
+    }
+}
+console.log("Titulo del issue: " + Issue.title)
+console.log("Fecha creada: "+ Issue.lastUpdate)
+console.log(Issue.getGeneralInfo())
