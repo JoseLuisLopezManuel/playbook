d537da84d515d895ce73809d6952974f06937b92
Jose Luis <launchx06519@innovaccion.mx>
2022-04-14 20:22:23 -0500

    Agregando 2.Ejemplo-forEachCalcular.js

---
+++ weekly_mission_2/example1/2.Ejemplo-forEachCalcular.js
@@ -0,0 +1,7 @@
+//ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
+let sum = 0;
+const number2 = [1, 2, 3, 4, 5];
+number2.forEach(num => sum = sum + num)
+console.log("Ejemplo 2: Cálculo de la suma de los elementos de la lista")
+console.log(sum)
+console.log(number2)
