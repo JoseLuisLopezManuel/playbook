1a3728a2dd6ba2009a8124e44aed5674ab733a61
Jose Luis <launchx06519@innovaccion.mx>
2022-04-14 20:28:50 -0500

    Agredando 3.Ejemplo-forEachImprimirMayus.js

---
+++ weekly_mission_2/example1/3.Ejemplo-forEachImprimirMayus.js
@@ -0,0 +1,4 @@
+//Ejemplo3: forEach para imprimir los países en letras mayusculas
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+console.log("Ejemplo 3: Imprimiendo la lista de países en mayuscula")
+countries.forEach((element) => console.log(element.toUpperCase()))
