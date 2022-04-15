1dced731dca3c22e74135a251f2482ba56cd92f2
Jose Luis <launchx06519@innovaccion.mx>
2022-04-15 11:38:57 -0500

    Agregando 5.MapConvertir.js

---
+++ weekly_mission_2/example1/5.MapConvertir.js
@@ -0,0 +1,5 @@
+const names = ['Asebenet', 'Mathias', 'Elias', 'Brook']
+const namesToUpperCase = names.map((name) => name.toUpperCase())
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
+console.log(namesToUpperCase)
+namesToUpperCase.forEach((name)=> console.log(name))
