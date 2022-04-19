f09c6809d1ff2a7f731397636795cdf3c14fd92e
Jose Luis <launchx06519@innovaccion.mx>
2022-04-18 20:18:34 -0500

    Mejorando Twitter.js

--- weekly_mission_2/exercises/exercise1/Twitter.js
+++ weekly_mission_2/exercises/exercise1/Twitter.js
@@ -15,3 +15,20 @@ const user = {
 console.log("Nombre y años del usuario: " + user.getuserandage())
 console.log(user.getGeneralInfo())
 console.log(user)
+
+const trending_topic = {
+    user: "Jose Luis",
+    username: "Jose_Luis",
+    Hastag: "#Jose",
+    email: "ejemplo@gmail.es",
+    friends:['@Lupita','@Carlos', '@Carla'],
+    getHastag: function(){
+        return this.Hastag
+    },
+    getGeneralInfo: function(){
+        return `this twitter account belongs to the user: ${this.username}, his age: ${this.age} and his email: ${this.email} and his Hastag is: ${this.Hastag}`
+    }
+}
+console.log("Su hastag es: " + trending_topic.getHastag())
+console.log(trending_topic.getGeneralInfo())
+console.log(trending_topic)
